c8603ea09ac2530571002fa984a5b6d19838d446
Todd Brunia <tbrunia@gmail.com>
2020-01-02 12:34:37 -0600

    feat: waiting for metrics apis before running helm

--- scripts/install-grafana.sh
+++ scripts/install-grafana.sh
@@ -2,6 +2,7 @@
 
 # reference: https://medium.com/@at_ishikawa/install-prometheus-and-grafana-by-helm-9784c73a3e97
 
+kubectl --namespace=kube-system wait --for=condition=Available --timeout=5m apiservices/v1beta1.metrics.k8s.io
 helm tiller run tiller -- helm install stable/grafana \
   --name grafana \
   --namespace monitoring \
--- scripts/install-prometheus.sh
+++ scripts/install-prometheus.sh
@@ -2,6 +2,7 @@
 
 # reference: https://docs.aws.amazon.com/eks/latest/userguide/prometheus.html
 
+kubectl --namespace=kube-system wait --for=condition=Available --timeout=5m apiservices/v1beta1.metrics.k8s.io
 helm tiller run tiller -- helm install stable/prometheus \
   --name prometheus \
   --namespace monitoring \
