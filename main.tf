afa1764b54c15b6b4a8358af31d697c9d5ed9ab4
Todd Brunia <tbrunia@gmail.com>
2020-02-17 16:03:56 -0600

    fix: fixed id

--- main.tf
+++ main.tf
@@ -301,7 +301,7 @@ resource "aws_cloudwatch_event_target" "giraffe-cloudwatch-event-target" {
     "Duration": 600000,
     "MetricDataQueries": [
       {
-        "Id": "client-metrics-reporter-lambda-duration",
+        "Id": "client_metrics_reporter_lambda_duration",
         "MetricStat": {
           "Metric": {
             "Namespace": "AWS/Lambda",
