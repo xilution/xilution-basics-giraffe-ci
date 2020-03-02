d50203bb55d35d682dad02817bdd122c02fd8508
Todd Brunia <tbrunia@gmail.com>
2020-03-01 20:42:38 -0600

    fix: added nginx ingress controller

--- scripts/install-nginx-ingress-controller.sh
+++ scripts/install-nginx-ingress-controller.sh
@@ -3,4 +3,5 @@
 # reference: https://kubernetes.github.io/ingress-nginx/deploy/#aws
 #            https://itnext.io/save-on-your-aws-bill-with-kubernetes-ingress-148214a79dcb
 
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.30.0/deploy/static/mandatory.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.30.0/deploy/static/provider/aws/service-nlb.yaml
