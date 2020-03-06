0d2253e8bad3e328040aaea9d135f21b3fadf1ce
Todd Brunia <tbrunia@gmail.com>
2020-03-06 08:10:42 -0600

    fix: fixed template user data

--- main.tf
+++ main.tf
@@ -300,10 +300,8 @@ locals {
   - apt-get -y install amazon-efs-utils
   - yum install -y nfs-utils
   - apt-get -y install nfs-common
-  - file_system_id_1=${aws_efs_file_system.nfs.id}
-  - efs_mount_point_1=/mnt/efs/fs1
-  - mkdir -p "\$\{efs_mount_point_1\}"
-  - test -f "/sbin/mount.efs" && echo "\$\{file_system_id_1\}:/ \$\{efs_mount_point_1\} efs tls,_netdev" >> /etc/fstab || echo "\$\{file_system_id_1\}.efs.us-east-1.amazonaws.com:/ \$\{efs_mount_point_1\} nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport,_netdev 0 0" >> /etc/fstab
+  - mkdir -p /mnt/efs/fs1
+  - test -f "/sbin/mount.efs" && echo "${aws_efs_file_system.nfs.id}:/ /mnt/efs/fs1 efs tls,_netdev" >> /etc/fstab || echo "${aws_efs_file_system.nfs.id}.efs.us-east-1.amazonaws.com:/ /mnt/efs/fs1 nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport,_netdev 0 0" >> /etc/fstab
   - test -f "/sbin/mount.efs" && echo -e "\n[client-info]\nsource=liw" >> /etc/amazon/efs/efs-utils.conf
   - mount -a -t efs,nfs4 defaults
   EOF
