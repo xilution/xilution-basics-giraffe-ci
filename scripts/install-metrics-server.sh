412d84b03f1c0841941a48ab43fd85af8e9fd96a
Todd Brunia <tbrunia@gmail.com>
2020-03-20 10:40:25 -0500

    chore: set metrics version

--- scripts/install-metrics-server.sh
+++ scripts/install-metrics-server.sh
@@ -2,8 +2,8 @@
 
 # reference: https://docs.aws.amazon.com/eks/latest/userguide/metrics-server.html
 
-DOWNLOAD_URL=$(curl --silent "https://api.github.com/repos/kubernetes-sigs/metrics-server/releases/latest" | jq -r .tarball_url)
-DOWNLOAD_VERSION=$(grep -o '[^/v]*$' <<< "$DOWNLOAD_URL")
+DOWNLOAD_VERSION="0.3.6"
+DOWNLOAD_URL="https://api.github.com/repos/kubernetes-sigs/metrics-server/tarball/v$DOWNLOAD_VERSION"
 curl -Ls "$DOWNLOAD_URL" -o metrics-server-"$DOWNLOAD_VERSION".tar.gz
 mkdir metrics-server-"$DOWNLOAD_VERSION"
 tar -xzf metrics-server-"$DOWNLOAD_VERSION".tar.gz --directory metrics-server-"$DOWNLOAD_VERSION" --strip-components 1
