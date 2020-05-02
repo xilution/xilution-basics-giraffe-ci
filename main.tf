821a1d06867f6563d6b606686ba8d44923a9e3e4
Todd Brunia <tbrunia@gmail.com>
2020-05-02 10:08:29 -0500

    chore: fixed data transfer bucket name

--- main.tf
+++ main.tf
@@ -101,7 +101,7 @@ resource "aws_ssm_parameter" "efs_filesystem_id" {
 
 locals {
   k8s_cluster_name = "xilution-giraffe-${substr(var.giraffe_pipeline_id, 0, 8)}"
-  k8s_data_transfer_bucket_name = "xilution-gazelle-${substr(var.gazelle_pipeline_id, 0, 8)}-data-transfer"
+  k8s_data_transfer_bucket_name = "xilution-giraffe-${substr(var.giraffe_pipeline_id, 0, 8)}-data-transfer"
 }
 
 resource "aws_s3_bucket" "k8s_data_transfer_bucket" {
