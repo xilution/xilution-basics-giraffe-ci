78ad1356e08ded3d96c218333503dc7fb9925ada
Todd Brunia <tbrunia@gmail.com>
2020-11-08 16:05:15 -0600

    fix: added gazelle pipe id variable

--- scripts/terraform-destroy-trunk.sh
+++ scripts/terraform-destroy-trunk.sh
@@ -2,6 +2,7 @@
 
 terraform destroy \
   -var="organization_id=$XILUTION_ORGANIZATION_ID" \
+  -var="gazelle_pipeline_id=$GAZELLE_PIPELINE_ID" \
   -var="giraffe_pipeline_id=$GIRAFFE_PIPELINE_ID" \
   -var="client_aws_account=$CLIENT_AWS_ACCOUNT" \
   -var="client_aws_region=$CLIENT_AWS_REGION" \
