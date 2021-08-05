0e91509525f14cef8c959ddfc3faec7d36610b04
Todd Brunia <tbrunia@gmail.com>
2021-08-04 21:34:05 -0500

    chore: fixed terraform apply

--- scripts/terraform-apply-trunk.sh
+++ scripts/terraform-apply-trunk.sh
@@ -3,17 +3,6 @@
 currentDir=$(pwd)
 cd ./terraform/trunk
 
-terraform apply -no-color \
-  -var="organization_id=$XILUTION_ORGANIZATION_ID" \
-  -var="gazelle_pipeline_id=$GAZELLE_PIPELINE_ID" \
-  -var="giraffe_pipeline_id=$GIRAFFE_PIPELINE_ID" \
-  -var="client_aws_account=$CLIENT_AWS_ACCOUNT" \
-  -var="client_aws_region=$CLIENT_AWS_REGION" \
-  -var="xilution_aws_account=$XILUTION_AWS_ACCOUNT" \
-  -var="xilution_aws_region=$XILUTION_AWS_REGION" \
-  -var="xilution_environment=$XILUTION_ENVIRONMENT" \
-  -var="xilution_pipeline_type=$PIPELINE_TYPE" \
-  -auto-approve \
-  ./terraform-plan.txt
+terraform apply -no-color -auto-approve ./terraform-plan.txt
 
 cd ${currentDir}
